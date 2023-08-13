c4e3bca3694ce1c6cfe9837ca8cf5852ec58755e
BarbedRuff <Barbed.Ruff@yandex.ru>
2023-08-13 18:40:49 +1000

    mmm

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+    maxWorkers: 1,
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
