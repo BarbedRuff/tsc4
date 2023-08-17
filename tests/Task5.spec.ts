c5be079d910f711a577cedad89fe8da479a008ce
BarbedRuff <Barbed.Ruff@yandex.ru>
2023-08-17 12:12:19 +1000

    go

--- tests/Task5.spec.ts
+++ tests/Task5.spec.ts
@@ -37,8 +37,8 @@ describe('Task5', () => {
     });
     it('fib', async () => {
         let res = await blockchain.runGetMethod(task5.address, 'fibonacci_sequence',
-        [{"type": "int", "value":370n},
-        {"type": "int", "value":0n}
+        [{"type": "int", "value":369n},
+        {"type": "int", "value":1n}
         ]
         );
         console.log(res.stack.at(0));
